aba1f623e204c27667bc999b08d6b5cba7ae5697
Francisco Dominguez Mateos <francisco.dominguez@urjc.es>
2017-08-25 03:37:15 +0200

    improvements

--- cOpenCV3BinnedDetector/src/binned_detector.h
+++ cOpenCV3BinnedDetector/src/binned_detector.h
@@ -20,12 +20,22 @@ public:
 	int MAX_COUNT;
 	int brows;
 	int bcols;
-	int minNpoints;
+	unsigned int minNpoints;
 	Ptr<FeatureDetector> detector;
-	BinnedDetector(int phr=4,int pwc=5,int mc=50):bwc(pwc),bhr(phr),MAX_COUNT(mc),minNpoints(5){
-		detector = ORB::create(MAX_COUNT);//number of features to detect
+	BinnedDetector(int phr=4,int pwc=5,int mc=50):bwc(pwc),bhr(phr),MAX_COUNT(mc),minNpoints(5),detector(ORB::create(MAX_COUNT)){//number of features to detect
 	}
 	virtual ~BinnedDetector(){}
+	vector<Point2f> pointsInBin(Mat &img,int i,int j,vector<Point2f> &pts2D){
+		vector<Point2f> rpts2D;
+		int brows=img.rows/bhr;
+		int bcols=img.cols/bwc;
+		Rect r(j, i, bcols, brows);
+		for(Point2f p:pts2D){
+			if(r.contains(p))
+				rpts2D.push_back(p);
+		}
+		return rpts2D;
+	}
 	int countPointsInBin(Mat &img,int i,int j,vector<Point2f> &pts2D){
 		int brows=img.rows/bhr;
 		int bcols=img.cols/bwc;
@@ -47,28 +57,51 @@ public:
 			dkpt[k].pt+=Point2f(j,i);
 		}
 	}
+	float dist(Point2f p1,Point2f p2){
+		Point2f pd=p2-p1;
+		return sqrt(pd.dot(pd));
+	}
 	void refreshDetection(Mat &img,vector<Point2f> &pts2D,vector<KeyPoint> &kpts){
+		unsigned int c=0;
 		int brows=img.rows/bhr;
 		int bcols=img.cols/bwc;
 		kpts.clear();
 		vector<KeyPoint> dkpt;
 		Mat ims;
-		for(int i=0;i<img.rows-brows;i+=brows/2)
-			for(int j=0;j<img.cols-bcols;j+=bcols/2){
-				if(countPointsInBin(img,i,j,pts2D)<minNpoints){
+		for(int i=0;i<img.rows-brows;i+=brows)
+			for(int j=0;j<img.cols-bcols;j+=bcols){
+				vector<Point2f> pts2Db;
+				pts2Db=pointsInBin(img,i,j,pts2D);
+				if(pts2Db.size()==0){
 					binDetect(img,i,j,dkpt);
-				    kpts.insert(kpts.end(), dkpt.begin(), dkpt.end());//append dkpt to kpts
+				    kpts.insert(kpts.end(), dkpt.begin(), dkpt.end());
+				    continue;
+				}
+				if(pts2Db.size()<minNpoints){
+					binDetect(img,i,j,dkpt);
+					//append dkpt to kpts without duplicates
+					for(KeyPoint &kp:dkpt){
+						for(Point2f pt:pts2Db){
+							if(dist(kp.pt,pt)>32.0){//this should be a attribute
+								kpts.push_back(kp);
+							}
+							else{
+								c++;
+								//cout << "discarded point "<<pt<<kp.pt<<endl;
+							}
+						}
+					}
 				}
 			}
+		cout << "discarded ="<<c<<endl;
 	}
 	void  binnedDetection(Mat &img,vector<KeyPoint> &kpts){
 		int brows=img.rows/bhr;
 		int bcols=img.cols/bwc;
 		kpts.clear();
 		vector<KeyPoint> dkpt;
-		Mat ims;
-		for(int i=0;i<img.rows-brows;i+=brows/2)
-			for(int j=0;j<img.cols-bcols;j+=bcols/2){
+		for(int i=0;i<img.rows-brows;i+=brows)
+			for(int j=0;j<img.cols-bcols;j+=bcols){
 				binDetect(img,i,j,dkpt);
 			    kpts.insert(kpts.end(), dkpt.begin(), dkpt.end());//append dkpt to kpts
 			}
@@ -79,7 +112,7 @@ public:
     TermCriteria termcrit;
     Size subPixWinSize;
 	BinnedGoodFeaturesToTrack(int phr=4,int pwc=5,int mc=10):BinnedDetector(phr,pwc,mc){
-		minNpoints=10;
+		minNpoints=2;
 		termcrit=TermCriteria(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
 		subPixWinSize=Size(10,10);
 	}
